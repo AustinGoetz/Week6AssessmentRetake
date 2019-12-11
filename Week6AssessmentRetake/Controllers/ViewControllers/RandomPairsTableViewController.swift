f25462b8145556c64c308b8856e8ab97d89b64f8
Austin Goetz <a.goetz09@gmail.com>
2019-12-11 16:22:44 -0700

    adds add person and randomize

--- Week6AssessmentRetake/Controllers/ViewControllers/RandomPairsTableViewController.swift
+++ Week6AssessmentRetake/Controllers/ViewControllers/RandomPairsTableViewController.swift
@@ -12,6 +12,8 @@ class RandomPairsTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        PersonController.shared.loadFromPersistentStore()
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -25,58 +27,47 @@ class RandomPairsTableViewController: UITableViewController {
     }
     
     @IBAction func randomizeButtonTapped(_ sender: Any) {
+        PersonController.shared.createRandomPairs()
+        self.tableView.reloadData()
     }
     
     // MARK: - Table view data source
 
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return PersonController.shared.pairs.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let sectionTitle = "Pair \(section + 1)"
+        
+        return sectionTitle
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return PersonController.shared.pairs[section].count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "personCell", for: indexPath)
 
-        // Configure the cell...
+        let pair = PersonController.shared.pairs[indexPath.section]
+        let personToDisplay = pair[indexPath.row]
+        
+        cell.textLabel?.text = personToDisplay.name
 
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-    
     // MARK: - Helpers
+    
     func presentAddPersonViewController() {
         let alertController = UIAlertController(title: "Add Person", message: "Add someone new to the list", preferredStyle: .alert)
         let addButton = UIAlertAction(title: "Add", style: .default) { (action) in
